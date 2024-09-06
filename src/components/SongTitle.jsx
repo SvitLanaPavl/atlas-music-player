95378c1b6e7520bff19f9c2ff80f0c5ac71db2e3
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-09-06 14:30:31 -0500

    added to config custom colors

--- src/components/SongTitle.jsx
+++ src/components/SongTitle.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const SongTitle = () => {
   return (
     <div>
-      <h2 className='mb-2 text-xl leading-8 font-bold'>Painted in Blue</h2>
-      <p className='mb-4 text-slate-500'>Soul Canvas</p>
+      <h2 className='mb-2 text-xl leading-8 font-bold text-customPurple-700'>Painted in Blue</h2>
+      <p className='mb-4 text-customPurple-200'>Soul Canvas</p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customPurple: {
+          50: '#F4EBFF',
+          100: '#D6BBFB',
+          200: '#9E77ED',
+          300: '#7F56D9',
+          400: '#6941C6',
+          500: '#53389E',
+          600: '#42307D',
+          700: '#261747',
+        }
+      }
+    },
   },
   plugins: [],
 }
